cc21c3721fc1630304207a7d56e81a5a84e2daa1
Michael Kunzmann <mail@michaelkunzmann.com>
2015-08-21 22:44:50 +0200

    new stuff

--- app/management/commands/pollmarkets.py
+++ app/management/commands/pollmarkets.py
@@ -22,16 +22,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        while True:
-            # Scanning w
-            print('>>> Scanning assets')
-            assets = Asset.objects.all()
+        print('>>> Scanning assets')
+        assets = Asset.objects.all()
 
-            for asset in assets:
-                json = yql('select * from yahoo.finance.quotes where symbol = "%s"' % asset.name)
-                quote = json['quote']
-                rate = Rate(asset=asset, date_time=timezone.now(), exchange=quote['StockExchange'], bid=quote['Bid'], ask=quote['Ask'], last=quote['LastTradePriceOnly'])
-                rate.save()
+        for asset in assets:
+            json = yql('select * from yahoo.finance.quotes where symbol = "%s"' % asset.name)
+            quote = json['quote']
+            rate = Rate(asset=asset, date_time=timezone.now(), exchange=quote['StockExchange'], bid=quote['Bid'], ask=quote['Ask'], last=quote['LastTradePriceOnly'])
+            rate.save()
 
-            print('>>> Successfully updated market data')
-            sleep(60)
+        print('>>> Successfully updated asset rates')
+        sleep(60)
