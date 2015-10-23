126c3d28ec59227a5bca20f1b20375bb9d630642
Michael Kunzmann <mail@michaelkunzmann.com>
2015-10-23 12:15:01 -0700

    new stuff

--- static/lib/controllers/JobDetailController.js
+++ static/lib/controllers/JobDetailController.js
@@ -2,14 +2,25 @@ app.controller('JobDetailCtrl', ['$scope', '$http', '$timeout', '$routeParams', 
 
 	$scope.jobId = $routeParams.jobId;
 
+	function formatJob() {
+		$scope.job.logsStr = $scope.job.logs.map(function (line) {
+			return '[' + line.timestamp + '] [' + line.streamType + '] ' + line.log;
+		}).reduce(function (prev, curr) {
+			return prev + curr;
+		})
+	}
+
 	$scope.reload = function () {
 		$http.get('/jobs/' + $scope.jobId)
 			.success(function (job) {
 				$scope.job = job;
+				formatJob();
 
 				appSocket.on('job_' + job._id, function (job) {
-					console.log('Job update')
+					console.log('Job update');
+
 					$scope.job = job;
+					formatJob();
 				});
 			})
 			.error(function (err) {
--- static/lib/controllers/JobsController.js
+++ static/lib/controllers/JobsController.js
@@ -2,15 +2,31 @@ app.controller('JobsCtrl', ['$scope', '$http', '$timeout', '$routeParams', 'appS
 
 	$scope.repoId = $routeParams.repoId;
 
+
+	function formatJob(job) {
+		job.logsStr = job.logs.map(function (line) {
+			return '[' + line.timestamp + '] [' + line.streamType + '] ' + line.log;
+		}).reduce(function (prev, curr) {
+			return prev + curr;
+		})
+	}
+
+
 	$scope.reload = function () {
 		$http.get('/repos/' + $scope.repoId + '/jobs')
 			.success(function (jobs) {
 				$scope.jobs = jobs;
+
+				$scope.jobs.forEach(function (job) {
+					formatJob(job);
+				});
 			})
 			.error(function (err) {
 				throw err;
 			});
 	}
+
+
 
 	$scope.reload();
 }]);
